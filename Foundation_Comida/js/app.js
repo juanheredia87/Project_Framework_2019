2f413163f94360f3163f234b67aaf2bdd48dbd8a
Juan Heredia <30888138+juanheredia87@users.noreply.github.com>
2019-11-10 17:49:12 -0500

    2019-11-10
    Culminacion proyecto Foundation

--- Foundation_Comida/js/app.js
+++ Foundation_Comida/js/app.js
@@ -1,3 +1,10 @@
 $(document).foundation()
 
 $("[data-menu-underline-from-center] a").addClass("underline-from-center");
+
+var $modal = $('#modal');
+
+$.ajax('/url')
+  .done(function(resp){
+    $modal.html(resp).foundation('open');
+});
